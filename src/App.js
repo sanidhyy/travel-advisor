3faf774effa6101e4f4521d849ce7bb063b3c925
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-06-18 22:53:13 +0530

    components placeholder created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+
 const App = () => {
   return (
     <div>
