0ebb10c23fad9097aefd474db4e28e532b3ee897
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-06-18 23:09:16 +0530

    Header style created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { CssBaseline, Grid } from "@material-ui/core";
 
 import Header from "./components/Header/Header";
 import List from "./components/List/List";
@@ -6,9 +7,19 @@ import Map from "./components/Map/Map";
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
   );
 };
 
