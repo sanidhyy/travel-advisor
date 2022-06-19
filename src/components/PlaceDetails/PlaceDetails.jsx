f9f203c87cca1793fbb128d328fec093a45f4f8c
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-06-19 19:01:37 +0530

    list styles added

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const PlaceDetails = () => {
+const PlaceDetails = ({ place }) => {
   return (
     <div>
-      <h1>PlaceDetails</h1>
+      <h1>{place.name}</h1>
     </div>
   );
 };
