8293fdc001c3732dffabf9dd7247d3072fa7fbde
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-06-20 11:51:22 +0530

    fixed minor bugs and issues

--- src/App.js
+++ src/App.js
@@ -32,13 +32,15 @@ const App = () => {
   }, [rating]);
 
   useEffect(() => {
-    setIsLoading(true);
-    getPlacesData(type, bounds.sw, bounds.ne).then((data) => {
-      setPlaces(data);
-      setFilteredPlaces([]);
-      setIsLoading(false);
-    });
-  }, [type, coordinates, bounds]);
+    if (bounds.sw && bounds.ne) {
+      setIsLoading(true);
+      getPlacesData(type, bounds.sw, bounds.ne).then((data) => {
+        setPlaces(data?.filter((place) => place.name && place.num_reviews > 0));
+        setFilteredPlaces([]);
+        setIsLoading(false);
+      });
+    }
+  }, [type, bounds]);
 
   return (
     <>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Autocomplete } from "@react-google-maps/api";
 import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 
@@ -19,8 +18,6 @@ const Header = () => {
           <Typography variant="h6" className={classes.title}>
             Explore new places
           </Typography>
-
-          {/* <Autocomplete> */}
           <div className={classes.search}>
             <div className={classes.searchIcon}>
               <SearchIcon />
@@ -31,7 +28,6 @@ const Header = () => {
               classes={{ root: classes.inputRoot, input: classes.inputInput }}
             />
           </div>
-          {/* </Autocomplete> */}
         </Box>
       </Toolbar>
     </AppBar>
